f30e96597bbe74f162e2c5f24014ebe5c078490f
Mario <luix30lp@gmail.com>
2021-08-15 16:03:17 -0500

    Update added animation

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -4,13 +4,14 @@ export const getGifs = async (category) => {
 	)}&limit=10&api_key=vlcAZZ0KdmwjAoj85DyUQW6WxjU5IsW9`;
 	const resp = await fetch(url);
 	const { data } = await resp.json();
+	console.log(data);
 	const gifs = data.map((gif) => {
 		return {
 			id: gif.id,
-			title: gif.title,
+			title: gif.title.length > 0 ? gif.title : gif.slug,
 			url: gif.images.downsized_medium.url,
 		};
 	});
-
+	console.log(gifs);
 	return gifs;
 };
